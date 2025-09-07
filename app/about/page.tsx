6035fc0d499f25556d8d1e29780b5864ef60473c
Máté Cserháti <mate.cserhati03@gmail.com>
2025-09-08 00:31:59 +0200

    feat: /about page to show short information about me

---
+++ app/about/page.tsx
@@ -0,0 +1,18 @@
+export default function About() {
+  return (
+    <div className="grid grid-cols-1 justify-items-center mx-auto mt-10 text-justify p-5 sm:p-0 sm:w-128    ">
+      <div>
+        <h1 className="font-bold underline text-2xl mb-2">About:</h1>
+        <p className="font-mono">
+          I'm a 21-year-old computer science student, expected to graduate in
+          2027. I'm passionate about software development, backend engineering,
+          and data analysis. In my free time, I build small projects to sharpen
+          my skills, primarily using ASP.NET Web API with MongoDB for REST APIs
+          and Python for data analysis. Recently, I've started exploring large
+          language models and machine learning to deepen my understanding of how
+          AI works.
+        </p>
+      </div>
+    </div>
+  );
+}
