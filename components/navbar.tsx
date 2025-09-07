f7273697402f148b3bc30f0871c1883ffe2ea188
Máté Cserháti <mate.cserhati03@gmail.com>
2025-09-08 00:33:55 +0200

    fix: using Link instead of <a>

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,20 +1,22 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
-      <nav className="pt-5">
-        <ul className="grid grid-cols-4 mx-auto justify-items-center">
-          <li className="hover:underline">
-            <a href="/">home</a>
-          </li>
-          <li className="hover:underline">
-            <a href="/about">about</a>
-          </li>
-          <li className="hover:underline">
-            <a href="/projects">projects</a>
-          </li>
-          <li className="hover:underline">
-            <a href="/resume">resume</a>
-          </li>
-        </ul>
-      </nav>
+    <nav className="pt-5 mx-auto w-full max-w-xs sm:max-w-none">
+      <ul className="grid grid-cols-4 mx-auto justify-items-center">
+        <li className="hover:underline">
+          <Link href="/">home</Link>
+        </li>
+        <li className="hover:underline">
+          <Link href="/about">about</Link>
+        </li>
+        <li className="hover:underline">
+          <Link href="/projects">projects</Link>
+        </li>
+        <li className="hover:underline">
+          <Link href="/resume">resume</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
