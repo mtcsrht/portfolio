702b3ddbad3cc04bc6c8a866c586aabbdd84a65e
Máté Cserháti <mate.cserhati03@gmail.com>
2025-10-20 20:44:21 +0200

    fix: adjust footer positioning for improved layout

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="fixed bottom-0 left-0 w-full text-center p-4">
+    <footer className="w-full text-center p-4 mt-10">
       <p className="font-mono">made by Mate Cserhati</p>
       <p className="font-mono">2025</p>
     </footer>
